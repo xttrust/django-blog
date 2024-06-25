6f7f229dd06c26ff9b2f901788ee926cae1e0e14
Florin Pinta <xttrust@live.com>
2024-06-25 19:52:58 +0000

    Setting up the env and config vars

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%#jtzz$7jl+j99dd2r8aknroa7q2$6q)zp2!-x(!y9th(xum6-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-xttrust-djangoblog-fjn3e4t8juo.ws.codeinstitute-ide.net', 
